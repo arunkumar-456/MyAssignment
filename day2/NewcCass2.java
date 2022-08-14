75b87b1ba776ccbd6b3d5f559fb8754175084a70
arunkumar-456 <110753048+arunkumar-456@users.noreply.github.com>
2022-08-14 14:09:18 +0530

    Add files via upload

---
+++ day2/NewcCass2.java
@@ -0,0 +1,30 @@
+package week2.day2;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class NewcCass2 {
+	
+	public static void main(String[] args)
+	
+	{
+		WebDriverManager.chromedriver().setup();
+		ChromeDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://erail.in/");
+		driver.findElement(By.xpath("//input[@id='txtStationFrom']")).clear();
+		driver.findElement(By.xpath("//input[@id='txtStationFrom']")).sendKeys("MS",Keys.TAB);
+		driver.findElement(By.xpath("//input[@id='txtStationTo']")).clear();
+		driver.findElement(By.xpath("//input[@id='txtStationTo']")).sendKeys("PY",Keys.TAB);
+		driver.findElement(By.xpath("//input[@type='checkbox']")).click();
+		driver.findElement(By.xpath("//*[@id=\"chkSelectDateOnly\"]")).click();
+		
+		
+		
+		
+	}
+
+}
